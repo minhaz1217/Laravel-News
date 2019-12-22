4c6192b19033b07906137f3c433466be2bacd11d
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-12-23 02:32:31 +0600

    middleware via controller to protect certain part of the website
    22

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::get( '/customers/{customer}', 'CustomerController@show');
 Route::patch( '/customers/{customer}', 'CustomerController@update');
 Route::get( '/customers/{customer}/edit', 'CustomerController@edit')->middleware('auth');
 Route::delete( '/customers/{customer}', 'CustomerController@destroy')->middleware('auth');
+//Route::get( '/customers/{customer}/edit', 'CustomerController@edit')->middleware('auth');
+//Route::delete( '/customers/{customer}', 'CustomerController@destroy')->middleware('auth');
 
 
 Route::get("/contact", "ContactFormController@create");
