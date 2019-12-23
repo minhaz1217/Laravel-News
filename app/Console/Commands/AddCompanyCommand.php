db7702f175c60be548b1fef633c922f79835303f
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-12-24 00:08:29 +0600

    interactive custom command
    35

--- app/Console/Commands/AddCompanyCommand.php
+++ app/Console/Commands/AddCompanyCommand.php
@@ -38,12 +38,25 @@ class AddCompanyCommand extends Command
      */
     public function handle()
     {
-        $company = Company::create([
-            'name' => $this->argument('name'),
-            'phone' => $this->argument('phone')??'N/A',
-        ]);
 
-        $this->info("Added: ". $this->argument('name'));
+        $name = $this->argument('name');
+        $phone = $this->argument('phone')??'N/A';
+
+        $name = $this->ask("What is the company name?");
+        $phone = $this->ask("What is the company phone number?");
+
+        if($this->confirm("Are you ready to insert" .$name ."?")){
+            $company = Company::create([
+                'name' => $name,
+                'phone' => $phone,
+            ]);
+            $this->info("Added Company: ". $name);
+        }else{
+            $this->info("Company wasn't added");
+        }
+
+
+
 //        $this->warn("This is a warning");
 //        $this->error("This is an error");
     }
