f625dc39e0f911286364536c577137f28c19a045
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-12-24 12:21:44 +0600

    run factory for foreign key concept
    38

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Company;
+use App\Customer;
+use Faker\Generator as Faker;
+
+$factory->define(Customer::class, function (Faker $faker) {
+    return [
+        'company_id' => factory(Company::class)->create(),
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'active' => 1,
+    ];
+});
